419d29d263393e5de7b47c657d14d3ed3f26e3c3
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-02-18 00:34:27 +0900

    Insert cover image to EPUB

--- build-book.sh
+++ build-book.sh
@@ -9,8 +9,8 @@ texdocumentclass_common=serial_pagination=true,openany,fontsize=10pt,baselineski
 texdocumentclass_ebook=media=ebook,paperwidth=152mm,paperheight=227mm,head_space=20mm
 texdocumentclass_print=media=print,paper=b5,head_space=30mm
 
-cover_pdf=''
-cover_epub='"coverimage":"images/tobira-ebook-01.png",'
+cover_pdf='"titlepage": true,'
+cover_epub='"coverimage":"images/tobira-ebook-01.png", "titlepage": false,'
 
 clear_duplicated_blanklines() {
   local DIR=$1
